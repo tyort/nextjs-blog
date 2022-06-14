f99ce53256afe6160fc92ed8676c89e86713372d
tyort <neotyo1986@gmail.com>
2022-06-14 09:08:59 +0300

    Use remark to convert markdown into HTML string

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,18 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
 
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
@@ -24,7 +28,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   // Fetch necessary data for the blog post using params.id
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
